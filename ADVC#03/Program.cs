5ab666d417532270107baf89b3f48388ae6cfc10
Ahmed El-dahy <ahmeddahy366-arch@gmail.com>
2026-09-03 21:26:11 +0300

    Quetions3

--- ADVC#03/Program.cs
+++ ADVC#03/Program.cs
@@ -21,33 +21,56 @@
 
             #endregion
             #region Quetions2
-          //  SortedDictionary<int,string> players=new SortedDictionary<int,string>();
-          //  {
-          //      players.Add(500, "Ahmed");
-          //      players.Add(200, "Sare");
-          //      players.Add(800, "Ali");
-          //      players.Add(350, "Mona");
-          //  }
-          //foreach(var player in players)
-          //  {
-          //      Console.WriteLine($"{player.Key}, {player.Value}");
-          //  }
-          //  Console.WriteLine(players.First());
-          //  Console.WriteLine(players.Any(x=>x.Key==500));
-          //  if (players.TryGetValue(900, out string? value))
-          //  {
-          //      Console.WriteLine($"Found player: {value}");
+            //  SortedDictionary<int,string> players=new SortedDictionary<int,string>();
+            //  {
+            //      players.Add(500, "Ahmed");
+            //      players.Add(200, "Sare");
+            //      players.Add(800, "Ali");
+            //      players.Add(350, "Mona");
+            //  }
+            //foreach(var player in players)
+            //  {
+            //      Console.WriteLine($"{player.Key}, {player.Value}");
+            //  }
+            //  Console.WriteLine(players.First());
+            //  Console.WriteLine(players.Any(x=>x.Key==500));
+            //  if (players.TryGetValue(900, out string? value))
+            //  {
+            //      Console.WriteLine($"Found player: {value}");
 
-          //  }
-          //  else
-          //  {
-          //      Console.WriteLine("Player with score 900 was not found");
-          //  }
-          //  players.Remove(200);
-          //  foreach (var player in players)
-          //  {
-          //      Console.WriteLine($"{player.Key}, {player.Value}");
-          //  }
+            //  }
+            //  else
+            //  {
+            //      Console.WriteLine("Player with score 900 was not found");
+            //  }
+            //  players.Remove(200);
+            //  foreach (var player in players)
+            //  {
+            //      Console.WriteLine($"{player.Key}, {player.Value}");
+            //  }
+
+
+            #endregion
+            #region Quetions3
+            //Dictionary<string, string> players2 = new Dictionary<string, string>()
+            //{
+            //    ["Ahmed"] = "0112890031",
+            //    ["Sara"] = "0112890032",
+            //    ["Ali"] = "0112890033",
+            //    ["Mona"] = "0112890034",
+            //};
+            //players["Mahmoud"]= "0112890035";
+            //foreach(string layer in players.Keys)
+            //{
+            //    Console.WriteLine($"Player: {layer}, Phone: {players[layer]}");
+            //}
+            ////players.Add("Ahmed", "0112890031" );
+            //players.TryAdd("Ahmed", "0112890031");
+            //bool isFound = players.ContainsKey("Hassan");
+            //Console.WriteLine($"Is Hassan in phonebook? {isFound}");
+            //Console.WriteLine(players.GetValueOrDefault("Hassan", "Not Found"));
+            //Console.WriteLine($"All playres: {string.Join(", ", players.Keys)}");
+            //Console.WriteLine($"All playres: {string.Join(", ", players.Values)}");
 
 
             #endregion
