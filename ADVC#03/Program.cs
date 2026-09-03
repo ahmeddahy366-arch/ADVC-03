e718591c3caf64993559bec8794b439573074a1a
Ahmed El-dahy <ahmeddahy366-arch@gmail.com>
2026-09-03 19:43:59 +0300

    Quetions1

--- ADVC#03/Program.cs
+++ ADVC#03/Program.cs
@@ -5,7 +5,21 @@
         static void Main(string[] args)
         {
 
-          
+            #region Quetions1 
+            //List<int> grads = new List<int> { 85, 92, 78, 95, 88, 70, 100, 65 };
+            //Console.WriteLine(grads.Count);
+            //Console.WriteLine(grads.First());
+            //Console.WriteLine(grads.Last());
+            //grads.Sort();
+            //Console.WriteLine(string.Join(", ", grads));
+            //Console.WriteLine(grads.Find(x => x > 90));
+            //Console.WriteLine(string.Join(", ", grads.FindAll(x => x < 75)));
+            //Console.WriteLine(grads.RemoveAll(x => x < 75));
+            //Console.WriteLine(grads.Any(x => x == 100));
+            //List<string> newGrads = grads.Select(x => $"Grade: {x}").ToList();
+            //Console.WriteLine(string.Join(", ", newGrads));
+
+            #endregion
         }
     }
 }
